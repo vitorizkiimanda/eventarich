f054a69f0e28d40d6f4e8ba2821069151383bce9
Vito Rizki Imanda <vitorizkiimanda@gmail.com>
2018-10-23 14:54:42 +0700

    register page ui

--- src/pages/login/login.ts
+++ src/pages/login/login.ts
@@ -1,5 +1,8 @@
 import { Component } from '@angular/core';
-import { NavController, NavParams } from 'ionic-angular';
+import { NavController, NavParams, AlertController, LoadingController } from 'ionic-angular';
+import { RegisterPage } from '../register/register';
+import { HomePage } from '../home/home';
+import { NgForm } from '@angular/forms';
 
 @Component({
   selector: 'page-login',
@@ -11,7 +14,11 @@ export class LoginPage {
   lihat = true;
   status = "password";
 
-  constructor(public navCtrl: NavController, public navParams: NavParams) {
+  constructor(
+    public navCtrl: NavController, 
+    public navParams: NavParams,
+    public loadCtrl: LoadingController,
+    public alertCtrl: AlertController) {
   }
 
   ionViewDidLoad() {
@@ -30,5 +37,55 @@ export class LoginPage {
     console.log(this.status);
   }
 
+  daftar(){
+    this.navCtrl.setRoot(RegisterPage);
+  }
+
+  comingSoon() {
+    const prompt = this.alertCtrl.create({
+      title: 'Forgot Password',
+      message: "Masukkan email untuk reset password",
+      inputs: [
+        {
+          name: 'email',
+          placeholder: 'email'
+        },
+      ],
+      buttons: [
+        {
+          text: 'Cancel',
+          handler: data => {
+            console.log('Cancel clicked');
+          }
+        },
+        {
+          text: 'Send',
+          handler: data => {
+            console.log('Saved clicked');
+          }
+        }
+      ]
+    });
+    prompt.present();
+  }
+
+  masuk(form: NgForm) {
+    
+    this.submitted = true;
+
+    let loading = this.loadCtrl.create({
+        content: 'memuat..'
+    });
+
+    if(form.valid){
+      
+      loading.present();    
+
+      setTimeout(() => { 
+        this.navCtrl.setRoot(HomePage)
+        loading.dismiss();     
+      }, 1000);
+    }
+  }
 
 }
--- src/pages/register/register.ts
+++ src/pages/register/register.ts
@@ -1,17 +1,62 @@
 import { Component } from '@angular/core';
-import { NavController, NavParams } from 'ionic-angular';
+import { NavController, NavParams, LoadingController } from 'ionic-angular';
+import { LoginPage } from '../login/login';
+import { HomePage } from '../home/home';
+import { NgForm } from '@angular/forms';
 
 @Component({
   selector: 'page-register',
   templateUrl: 'register.html',
 })
 export class RegisterPage {
+  
+  submitted = false;
+  lihat = true;
+  status = "password";
 
-  constructor(public navCtrl: NavController, public navParams: NavParams) {
+  constructor(
+    public navCtrl: NavController, 
+    public navParams: NavParams,
+    public loadCtrl: LoadingController) {
   }
 
   ionViewDidLoad() {
     console.log('ionViewDidLoad RegisterPage');
+  }
+
+  showPassword(){
+    this.status = "text";
+    this.lihat = false;
+    console.log(this.status);
+  }
+
+  hidePassword(){
+    this.status = "password";
+    this.lihat = true;
+    console.log(this.status);
+  }
+
+  masuk(){
+    this.navCtrl.setRoot(LoginPage);
+  }
+
+  daftar(form: NgForm) {
+    
+    this.submitted = true;
+
+    let loading = this.loadCtrl.create({
+        content: 'memuat..'
+    });
+
+    if(form.valid){
+      
+      loading.present();    
+
+      setTimeout(() => { 
+        this.navCtrl.setRoot(HomePage)
+        loading.dismiss();     
+      }, 1000);
+    }
   }
 
 }
